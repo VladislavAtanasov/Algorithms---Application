f3a103d226952128916c082baa2dd662357f950b
VladislavAtanasov <barcabg95@abv.bg>
2015-06-02 22:08:07 +0300

    Adding Encryption

--- Encrypt-Message/encrypt.py
+++ Encrypt-Message/encrypt.py
@@ -12,15 +12,39 @@ def encrypt(string):
     print(key)
     alphabet = sehalf[sehalf.index("~") + 1: sehalf.index("~") + lengalpha + 1]
     print(alphabet)
-    encryptalphabet = reversestring[reversestring.index(alphabet[len(alphabet)-1])+1:reversestring.index(alphabet[len(alphabet)-1])+18]
+    encryptalphabet = reversestring[reversestring.index(alphabet[len(alphabet)-1])+1:reversestring.index(alphabet[len(alphabet)-1])+len(alphabet) + 2]
     print(encryptalphabet)
     indexes = []
     for x in encryptalphabet:
         indexes.append(alphabet.index(x))
     print(indexes)
-    print(x/16 + x%16 )
+    stringofkey = ""
+    while len(stringofkey) < len(encryptalphabet):
+        stringofkey += key
+    stringofkey = stringofkey[:len(encryptalphabet)]
+    print(stringofkey)
+    indexes_of_key = []
+    for elem in stringofkey:
+        indexes_of_key.append(alphabet.index(elem))
+    print(indexes_of_key)
+    indexes_of_word = []
+    for ind in range(len(encryptalphabet)):
+        new_index = indexes[ind] - indexes_of_key[ind]
+        if new_index < 0:
+            word_index = new_index + lengalpha
+            indexes_of_word.append(word_index)
+        else:
+            indexes_of_word.append(new_index)
+    print(indexes_of_word)
+    word = ""
+    for elem in indexes_of_word:
+        print(encryptalphabet[elem])
+        word += alphabet[elem]
+    print(word)
+
+
 def main():
-    encrypt("vrItommseIal vihack~416~Ilocveakgrithms he")
+    encrypt("o?uin uw?stutnfwat?~413~orwa? thfuisnnrsiu")
 
 if __name__ == '__main__':
     main()
