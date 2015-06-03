c8985f9eca6904e3e2a75c158ad8a72794ed5794
VladislavAtanasov <barcabg95@abv.bg>
2015-06-03 22:58:06 +0300

    Adding Brackets Task

--- Brackets/brackets.py
+++ Brackets/brackets.py
@@ -1,32 +1,86 @@
+def isOpening(brace):
+        return brace == '{' or brace == '[' or brace == '('
+
+
+def isClosing(brace):
+        return brace == ')' or brace == ']' or brace == '}'
+
+
+def isEmpty(stack):
+    return len(stack) == 0
+
+
+def sumUntilOpening(stack):
+        suma = 0
+        base = 1
+        temp = stack.pop()
+        while isOpening(temp[0]) is False:
+            if temp[0] == '+':
+                suma += int(temp)
+                base = 1
+            else:
+                suma += base * int(temp)
+                base *= 10
+            if isEmpty(stack):
+                break
+            else:
+                temp = stack.pop()
+        if not isEmpty(stack):
+            suma = 2 * suma
+        stack.append("+" + str(suma))
+
+
+def evaluate(expr):
+    stack = []
+    for i in range(len(expr)):
+        if isOpening(expr[i]) or isinstance(expr[i], int):
+            stack.append(expr[i])
+        elif isClosing(expr[i]):
+            sumUntilOpening(stack)
+    return int(stack.pop())
+
+
 def bracket_validation(string):
-    opencurls = 0
-    opensquar = 0
-    opencircl = 0
+    stack = []
     for x in string:
         if x == "{":
-            opencurls += 1
-        elif x == "}":
-            opencurls -= 1
+            stack.append(x)
         elif x == "(":
-            opencircl += 1
-        elif x == ")":
-            opencircl -= 1
+            stack.append(x)
         elif x == "[":
-            opensquar += 1
-        elif x == "]":
-            opensquar -= 1
-    if opensquar == 0 and opencircl == 0 and opencurls == 0:
-        if "{" in string:
-            if string.index('{') != 0:
-                return "NO"
-            else:
-                pass
-    else:
-        return "NO"
+            stack.append(x)
+    if "(" in string and "{" in string:
+        if string.index("(") < string.index('{') and string.index("}") < string.index(")"):
+            return "NO"
+    if "(" in string and "[" in string:
+        if string.index("(") < string.index('[') and string.index("]") < string.index(")"):
+            return "NO"
+    if "[" in string and "{" in string:
+        if string.index("[") < string.index("{"):
+            return "NO"
+    if '{' in string:
+        if stack.count('{') > 1 or stack.index("{") != 0:
+            return "NO"
+    if "(" in stack and "{" in stack:
+        if stack.index("{") == stack.index("(") - 1:
+            return "NO"
+    while len(stack) != 0:
+        for x in string:
+            if x in ["]", "}", ")"]:
+                if x == ")" and stack[len(stack) - 1] == "(":
+                    stack.pop()
+                elif x == "]" and stack[len(stack) - 1] == "[":
+                    stack.pop()
+                elif x == "}" and stack[len(stack) - 1] == "{":
+                    stack.pop()
+                else:
+                    return "NO"
+    return evaluate(string)
 
 
 def main():
-    print(bracket_validation("{125[12]{125}[12]125}"))
+    print(bracket_validation("[123(145)38(37)812]"))
+    print(bracket_validation("{125[2][(1)][3]125}"))
 
 if __name__ == '__main__':
     main()
